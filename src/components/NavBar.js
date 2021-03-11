ec9a26f99322411f2c50de7672763f6f08a65995
Ruby <s110619049@stu.ntue.edu.tw>
2021-03-11 11:12:47 +0800

    grid navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+export default function NavBar() {
+    return (
+        <navbar className="navBar">
+            <p className="navbarlist">
+                Tableware
+            </p>
+            <p className="navbarlist">
+                Cookware
+            </p>
+            <p className="navbarlist">
+                Home accessories
+            </p>
+            <p className="navbarlist">
+                Lighting
+            </p>
+            <p className="navbarlist">
+                Textile
+            </p>
+            <p className="navbarlist">
+                Furniture
+            </p>
+        </navbar>
+
+    )
+}
